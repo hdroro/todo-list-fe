f2a6d04cb32adec0986559a472d15f387591d89f
hongdiem_roro <mingukmanse673@gmail.com>
2024-02-13 12:46:44 +0700

    apptoday

--- src/components/AppToday/AppToday.js
+++ src/components/AppToday/AppToday.js
@@ -1,17 +1,64 @@
-import { CheckedIcon } from "../Icon/Icon";
+import { useState } from "react";
+import {
+  CheckedIcon,
+  ChevRonDownIcon,
+  CircleIcon,
+  DueDate,
+  EditIcon,
+} from "../Icon/Icon";
 import "./AppToday.scss";
 
 function AppToday() {
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleSwitchToEdit = () => {
+    setIsEdit(true);
+  };
   return (
     <div className="container">
       <div className="today-content">
         <h3>Today</h3>
-        <div className="count-tasks">
+        <div className="count-tasks d-flex display align-items-center">
           <CheckedIcon />
           <span className="num-of-tasks">2 </span>
           tasks
         </div>
-        <div className="form-group"></div>
+        <div className="form-group over-due">
+          <h6>
+            <ChevRonDownIcon />
+            <span>Overdue</span>
+          </h6>
+          <div className="content-over-due d-flex mt-2">
+            <CircleIcon className="mt-1" />
+            <div className="group-item">
+              <input
+                className={`input-title-task border-0`}
+                type="text"
+                placeholder="Task name"
+                value={taskName}
+                onChange={(event) => setTaskName(event.target.value)}
+                readOnly={!isEdit}
+              />
+              <input
+                className={`input-description-task border-0`}
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+                readOnly={!isEdit}
+              />
+              <button className="due-date">
+                <DueDate />8 Feb
+              </button>
+            </div>
+            <EditIcon
+              className="edit-action"
+              onClick={() => handleSwitchToEdit()}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -195,9 +195,9 @@ export const CheckedIcon = ({
   >
     <path
       fill="currentColor"
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8 14.001a6 6 0 1 1 0-12 6 6 0 0 1 0 12Zm0-1a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM5.146 8.147a.5.5 0 0 1 .708 0L7 9.294l3.146-3.147a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 0-.708Z"
-      clip-rule="evenodd"
+      clipRule="evenodd"
     ></path>
   </svg>
 );
@@ -218,9 +218,9 @@ export const RoundedIcon = ({
   >
     <path
       fill="currentColor"
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M8 14.001a6 6 0 1 1 0-12 6 6 0 0 1 0 12Zm0-1a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM5.146 8.147a.5.5 0 0 1 .708 0L7 9.294l3.146-3.147a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 0-.708Z"
-      clip-rule="evenodd"
+      clipRule="evenodd"
     ></path>
   </svg>
 );
@@ -240,5 +240,59 @@ export const CheckIcon = ({
     viewBox="0 0 448 512"
   >
     <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+  </svg>
+);
+
+export const CircleIcon = ({
+  width = "16",
+  height = "16",
+  className,
+  onClick,
+}) => (
+  <svg
+    className={className}
+    width={width}
+    height={height}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
+  </svg>
+);
+
+export const ChevRonDownIcon = ({
+  width = "16",
+  height = "16",
+  className,
+  onClick,
+}) => (
+  <svg
+    className={className}
+    width={width}
+    height={height}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+  </svg>
+);
+
+export const EditIcon = ({
+  width = "16",
+  height = "16",
+  className,
+  onClick,
+}) => (
+  <svg
+    className={className}
+    width={width}
+    height={height}
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
   </svg>
 );
