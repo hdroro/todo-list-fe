d059d844d46bad7c25c5748fa23a4fa2e8e2e208
hongdiem_roro <mingukmanse673@gmail.com>
2024-02-14 20:13:29 +0700

    setup redux-toolkit

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchAllUsers = createAsyncThunk(
+  "users/fetchAllUsers",
+  async () => {
+    const response = await axios.get("http://localhost:8080/users/all");
+    return response.data;
+  }
+);
+
+const initialState = {
+  listUsers: [],
+  isLoading: false,
+  isError: false,
+};
+
+export const userSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllUsers.pending, (state, action) => {
+        state.isLoading = true;
+        state.isError = false;
+      })
+      .addCase(fetchAllUsers.fulfilled, (state, action) => {
+        state.listUsers = action.payload;
+        state.isLoading = false;
+        state.isError = false;
+      })
+      .addCase(fetchAllUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+      });
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
+
+export default userSlice.reducer;
